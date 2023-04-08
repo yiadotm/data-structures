05827de6b671ce7e42820c6f5d734fcc1a3787ff
Danee Dang <dudang@ucsc.edu>
2023-04-07 18:01:59 -0700

    ongoing

--- pa1/List.c
+++ pa1/List.c
@@ -17,6 +17,7 @@ typedef struct NodeObj* Node;
 // private NodeObj type
 typedef struct NodeObj{
    ListElement data;
+   Node prev;
    Node next;
 } NodeObj;
 
@@ -36,7 +37,7 @@ Node newNode(ListElement data){
    Node N = malloc(sizeof(NodeObj));
    assert( N!=NULL );
    N->data = data;
-   N->next = NULL;
+   N->next = N->prev = NULL;
    return(N);
 }
 
@@ -65,7 +66,7 @@ List newList(){
 void freeList(List* pL){
    if(pL!=NULL && *pL!=NULL) { 
       while( !isEmpty(*pL) ) { 
-         clear(*pL); 
+         deleteFront(*pL); 
       }
       free(*pL);
       *pL = NULL;
@@ -147,7 +148,23 @@ int get(List L) {
 // equals()
 // Returns true iff Lists A and B are in same state, and returns false otherwise.
 bool equals(List A, List B) {
+   if( A==NULL || B==NULL ){
+      printf("List Error: calling equals() on NULL List reference\n");
+      exit(EXIT_FAILURE);
+   }
 
+   bool eq;
+   Node N, M;
+
+   eq = ( A->length == B->length );
+   N = A->front;
+   M = B->front;
+   while( eq && N!=NULL){
+      eq = ( N->data==M->data );
+      N = N->next;
+      M = M->next;
+   }
+   return eq;
 }
 
 // isEmpty()
@@ -161,3 +178,61 @@ bool isEmpty(List L){
 }
 
 // Manipulation procedures ----------------------------------------------------
+
+// clear()
+// Resets L to its original empty state.
+void clear(List L) {
+   if( isEmpty(L) ){
+      return;
+   }  
+   L->front = L->back = L->cursor= NULL;
+   L->length = L->index = 0;
+
+}
+// set()
+// Overwrites the cursor element’s data with x
+// Pre: length()>0, index()>=0
+void set(List L, int x) {
+   if (L->length < 0 || L->index < 0) {
+      printf("List Error: calling set() with out of bounds parameter.\n");
+      exit(EXIT_FAILURE);
+   }
+   L->cursor->data = x;
+
+}
+
+// moveFront()
+// If L is non-empty, sets cursor under the front element, otherwise does nothing.
+void moveFront(List L) {
+   if( isEmpty(L) ){
+      return;
+   }
+   L->front->next = L->cursor;
+
+}
+
+// moveBack()
+// If L is non-empty, sets cursor under the back element, otherwise does nothing.
+void moveBack(List L) {
+   if( isEmpty(L) ){
+      return;
+   }
+   L->back->next = L->cursor;
+
+}
+// movePrev()
+// If cursor is defined and not at front, move cursor one
+// step toward the front of L; if cursor is defined and at
+// front, cursor becomes undefined; if cursor is undefined
+// do nothing
+void movePrev(List L) {
+   if (L->index != -1 && L->cursor != L->front) {
+      L->index--;
+      L->cursor = L->cursor->prev;
+   }
+
+   if (L->index != -1 && L->cursor == L->front) {
+      L->index = -1;
+   }
+}
+
