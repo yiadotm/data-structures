7946901dab96882a00be358c2e7a5d5d5f64e4fd
Danee <dudang@ucsc.edu>
2023-04-10 03:45:26 -0700

    finish the cursor stuff

--- pa1/List.c
+++ pa1/List.c
@@ -257,19 +257,92 @@ void moveNext(List L) {
 // Insert new element into L. If L is non-empty,
 // insertion takes place before front element.
 void prepend(List L, int x) {
-   Node N = newNode(x);
 
-   if( L==NULL ){
-      printf("Queue Error: calling Enqueue() on NULL Queue reference\n");
-      exit(EXIT_FAILURE);
-   }
-   
+
+   Node N = newNode(x);  
    if( isEmpty(L) ) { 
-      L->front = L->back = N; 
-   }else{ 
-      L->front->next = N; 
-      L->front = N; 
+      N->next = L->front;
+      L->front = N;
+      return;
+   }
+   N->next = L->front;
+   N->prev = NULL;
+
+   if (L->front != NULL) {
+      L->front->prev = N;
    }
    L->length++;
 }
 
+// append()
+// Insert new element into L. If L is non-empty,
+// insertion takes place after back element.
+void append(List L, int x) {
+   Node N = newNode(x); 
+
+   if( isEmpty(L) ) { 
+      N->prev = NULL;
+      L->front = N;
+      return;
+   }
+   while (L->back->next != NULL) { 
+      L->back = L->back->next; 
+      L->back->next = N;
+      N->prev = L->back;
+   }
+
+   L->length++;
+}
+
+// insertBefore()
+// Insert new element before cursor.
+// Pre: length()>0, index()>=0
+void insertBefore(List L, int x) {
+
+}
+
+// insertAfter()
+// Insert new element after cursor.
+// Pre: length()>0, index()>=0
+void insertAfter(List L, int x) {
+
+}
+
+// deleteFront()
+// Delete the front element. Pre: length()>0
+void deleteFront(List L) {
+
+}
+
+// deleteBack()
+// Delete the back element. Pre: length()>0
+void deleteBack(List L) {
+
+}
+
+// delete()
+// Delete cursor element, making cursor undefined.
+// Pre: length()>0, index()>=0
+void delete(List L) {
+
+}
+
+// Other operations -----------------------------------------------------------
+
+// printList()
+// Prints to the file pointed to by out, a
+// string representation of L consisting
+// of a space separated sequence of integers,
+// with front on left.
+void printList(FILE* out, List L) {
+
+}
+
+// copyList()
+// Returns a new List representing the same integer
+// sequence as L. The cursor in the new list is undefined,
+// regardless of the state of the cursor in L. The state
+// of L is unchanged.
+List copyList(List L) {
+   
+}
