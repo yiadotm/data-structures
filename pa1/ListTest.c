6c6ee8a9a9203a2569402390f3125f78259859c3
Danee Dang <dudang@ucsc.edu>
2023-04-13 17:54:55 -0700

    list test

--- pa1/ListTest.c
+++ pa1/ListTest.c
@@ -1,7 +1,10 @@
-/****************************************************************************************
-*  ListClient.c
-*  Test client for List ADT
-*****************************************************************************************/
+/********************************************************************************* 
+* Danee Dang, dudang
+* 2023 Spring CSE101 PA1
+* ListTest.c
+* File that tests the List ADT
+*********************************************************************************/ 
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
@@ -12,8 +15,10 @@ int main(int argc, char* argv[]){
    List A = newList();
    List B = newList();
    List C = NULL;
-   // List Piyush = newList();
+   //List owen = newList();
    int i;
+   char * app = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20";
+   char * pre = "20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1";
    // printf("Created Lists.\n");
    // List O = newList();
    // append(O, 1);
@@ -50,46 +55,75 @@ int main(int argc, char* argv[]){
       //printf("print good.\n");
       prepend(B,i);
    }
-   //printf("appended and prepended.\n");
+   printf("Check append() and prepend()\n\n");
+   printf("List A: ");
    printList(stdout,A); 
-   printf("\n");
+   printf("Compare with actual results: ");
+   printf("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20");
+   printf("\n\n");
+   printf("List B: ");
    printList(stdout,B); 
-   printf("\n");
+   printf("Compare with actual results: ");
+   printf("20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1");
+   printf("\n\n");
 
+   //check for moveFront, index, moveNext
+   printf("Check moveFront(), index(), and moveNext()\n\n");
    for(moveFront(A); index(A)>=0; moveNext(A)){
       //int i;
       //printf("loop %d\n", i++);
       //printf("%d\n", index(A));
       printf("%d ", get(A));
    }
-   printf("\n");
+   printf("Compare with actual results: ");
+   printf("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20");
+   printf("\n\n");
+   //check for moveBack, index, movePrev
+   printf("Check moveBack(), index(), and movePrev()");
    for(moveBack(B); index(B)>=0; movePrev(B)){
       printf("%d ", get(B));
    }
-   printf("\n");
+   printf("Compare with actual results: ");
+   printf("20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1");
+   printf("\n\n");
    //printf("index: %d\n", index(A));
-   C = copyList(A);
-   printf("%s\n", equals(A,B)?"true":"false");
-   printf("%s\n", equals(B,C)?"true":"false");
-   printf("%s\n", equals(C,A)?"true":"false");
+
+   C = copyList(A);                                   //test for copyList
+   //printf("%s\n", equals(A,B)?"true":"false");      
+   assert(!equals(A,B));                              //should be false
+   //printf("%s\n", equals(B,C)?"true":"false");
+   assert(!equals(B,C));                              //should be false
+   //printf("%s\n", equals(C,A)?"true":"false");
+   assert(equals(C, A));                              //should be true
 
 
+   //test the inserts
    moveFront(A);
+   assert(front(A) == 1);                             //front should be 1
+   assert(back(A) == 20);                             //back should be 20
    for(i=0; i<5; i++) moveNext(A); // at index 5
    // printf("index: %d\n", index(A));
    // printf("cursor: %d\n", get(A));
-   insertBefore(A, -1);            // at index 6
+   insertBefore(A, -1);            // at index 6      //insertBefore()
    for(i=0; i<9; i++){ moveNext(A); } // at index 15 
-   insertAfter(A, -2);
+   insertAfter(A, -2);                                //insertAfter
    //printList(stdout, A);
    //printf("cursor: %d\n", get(A));
    for(i=0; i<5; i++) movePrev(A); // at index 10
-   delete(A);
+   
+
+   //Test the deletes                                                  
+   delete(A);                                         //deletes 10
+   deleteFront(A);                                    //deletes 1
+   deleteBack(A);                                     //deletes 20
    printList(stdout,A);
    //printf("\n");
-   printf("%d\n", length(A));
+
+   //printf("%d\n", length(A));
+   assert(length(A) == 19);                           //length should be 19
    clear(A);
-   printf("%d\n", length(A));
+   //printf("%d\n", length(A));
+   assert(length(A) == 0);                            //length should be 0
 
    // append(Piyush, 2);
    // append(Piyush, 3);
@@ -121,13 +155,13 @@ int main(int argc, char* argv[]){
 /*
 Output of this program:
 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+
 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+
 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+
 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-false
-false
-true
-1 2 3 4 5 -1 6 7 8 9 11 12 13 14 15 -2 16 17 18 19 20
-21
-0
+
+2 3 4 5 -1 6 7 8 9 11 12 13 14 15 -2 16 17 18 19
+
 */
