040bf7e86d75794daa6dd588f6c23ba47a279799
root <root@Yun-Laptop.localdomain>
2023-05-19 02:21:28 -0700

    in progress

--- pa6/BigInteger.cpp
+++ pa6/BigInteger.cpp
@@ -9,3 +9,194 @@
 #include"List.h"
 #include "BigInteger.h"
 
+// Class Constructors & Destructors -------------------------------------------
+// BigInteger()
+// Constructor that creates a new BigInteger in the zero state: 
+// signum=0, digits=().
+BigInteger::BigInteger() {
+    signum = 0;
+    digits = new List();
+}
+
+// BigInteger()
+// Constructor that creates a new BigInteger from the long value x.
+BigInteger::BigInteger(long x) {
+
+}
+
+
+// BigInteger()
+// Constructor that creates a new BigInteger from the string s.
+// Pre: s is a non-empty string consisting of (at least one) base 10 digit
+// {0,1,2,3,4,5,6,7,8,9}, and an optional sign {+,-} prefix.
+BigInteger::BigInteger(std::string s) {
+
+}
+
+// BigInteger()
+// Constructor that creates a copy of N.
+BigInteger::BigInteger(const BigInteger& N) {
+    signum = N.signum;
+    digits = N.digits;
+
+}
+
+
+// Access functions --------------------------------------------------------
+
+// sign()
+// Returns -1, 1 or 0 according to whether this BigInteger is positive, 
+// negative or 0, respectively.
+int BigInteger::sign() const {
+    return (signum);
+}
+
+// compare()
+// Returns -1, 1 or 0 according to whether this BigInteger is less than N,
+// greater than N or equal to N, respectively.
+int BigInteger::compare(const BigInteger& N) const {
+
+}
+
+// Manipulation procedures -------------------------------------------------
+
+// makeZero()
+// Re-sets this BigInteger to the zero state.
+void BigInteger::makeZero() {
+    signum = 0;
+    digits.clear();
+}
+
+// negate()
+// If this BigInteger is zero, does nothing, otherwise reverses the sign of 
+// this BigInteger positive <--> negative. 
+void BigInteger::negate() {
+    if (signum == 0) {
+        return;
+    }
+    return (signum * -1);
+}
+
+
+// BigInteger Arithmetic operations ----------------------------------------
+
+// add()
+// Returns a BigInteger representing the sum of this and N.
+BigInteger BigInteger::add(const BigInteger& N) const {
+
+}
+
+// sub()
+// Returns a BigInteger representing the difference of this and N.
+BigInteger BigInteger::sub(const BigInteger& N) const {
+
+}
+
+// mult()
+// Returns a BigInteger representing the product of this and N. 
+BigInteger BigInteger::mult(const BigInteger& N) const {
+
+}
+
+// to_string()
+// Returns a string representation of this BigInteger consisting of its
+// base 10 digits. If this BigInteger is negative, the returned string 
+// will begin with a negative sign '-'. If this BigInteger is zero, the
+// returned string will consist of the character '0' only.
+std::string BigInteger::to_string() {
+
+}
+
+// Overriden Operators -----------------------------------------------------
+
+// operator<<()
+// Inserts string representation of N into stream.
+std::ostream& operator<<( std::ostream& stream, BigInteger N ) {
+    return stream << N.BigInteger::to_string();
+}
+
+// operator==()
+// Returns true if and only if A equals B. 
+bool operator==( const BigInteger& A, const BigInteger& B ) {
+    if (A.BigInteger::compare(B) == 0) {
+        return true;
+    }
+    return false;
+}
+
+// operator<()
+// Returns true if and only if A is less than B. 
+bool operator<( const BigInteger& A, const BigInteger& B ) {
+    if (A.BigInteger::compare(B) == -1) {
+        return true;
+    }
+    return false;
+}
+
+// operator<=()
+// Returns true if and only if A is less than or equal to B. 
+bool operator<=( const BigInteger& A, const BigInteger& B ) {
+    int x = A.BigInteger::compare(B);
+    if (x == -1 || x == 0) {
+        return true;
+    }
+    return false;
+}
+
+// operator>()
+// Returns true if and only if A is greater than B. 
+bool operator>( const BigInteger& A, const BigInteger& B ) {
+    if (A.BigInteger::compare(B) == 1) {
+        return true;
+    }
+    return false;
+}
+
+// operator>=()
+// Returns true if and only if A is greater than or equal to B. 
+bool operator>=( const BigInteger& A, const BigInteger& B ) {
+    int x = A.BigInteger::compare(B);
+    if (x == 1 || x = 0) {
+        return true;
+    }
+    return false;
+}
+
+// operator+()
+// Returns the sum A+B. 
+BigInteger operator+( const BigInteger& A, const BigInteger& B ) {
+    return (A.BigInteger::add(B));
+}
+
+// operator+=()
+// Overwrites A with the sum A+B. 
+BigInteger operator+=( BigInteger& A, const BigInteger& B ) {
+    A = A.BigInteger::add(B);
+    return (A);
+}
+
+// operator-()
+// Returns the difference A-B. 
+BigInteger operator-( const BigInteger& A, const BigInteger& B ) {
+    return (A.BigInteger::sub(B));
+}
+
+// operator-=()
+// Overwrites A with the difference A-B. 
+BigInteger operator-=( BigInteger& A, const BigInteger& B ) {
+    A = A.BigInteger::sub(B);
+    return (A);
+}
+
+// operator*()
+// Returns the product A*B. 
+BigInteger operator*( const BigInteger& A, const BigInteger& B ) {
+    return (A.BigInteger::mult(B));
+}
+
+// operator*=()
+// Overwrites A with the product A*B. 
+BigInteger operator*=( BigInteger& A, const BigInteger& B ) {
+    A = A.BigInteger::mult(B);
+    return(A);
+}
